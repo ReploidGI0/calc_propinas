c113d0ab3813b684d3223033d60845dfebcb9644
ReploidGI0 <gilespericueta@gmail.com>
2025-04-16 14:49:22 -0700

    Se añadió la función para agregar un item a la orden, evita duplicados (actualiza quantity)

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -5,8 +5,22 @@ export default function useOrder() {
     const [order, setOrder] = useState<OrderItem[]>([])
 
     const addItem = (item : MenuItem) => {
-        console.log(item)
+        const itemExist = order.find(orderItem => orderItem.id === item.id)
+        if(itemExist){
+            //Compara cual es el elemento repetido por medio de id's, se usa map para crear una copia del arreglo (cuestiones de mutabildiad)
+            const updatedOrder = order.map(orderItem => orderItem.id === item.id ? 
+                {...orderItem, quantity: orderItem.quantity +1} : //Esto toma una copia de lo que YA HAY EN LA ORDEN pero en la cantidad toma lo que ya hay de cantidad y lo incrementa en 1
+                orderItem //Esto retorna los elementos que NO SON IGUALES al que se esta agregando (no duplicados)
+            )
+            setOrder(updatedOrder) 
+        } else{
+            const newItem = {...item, quantity : 1}
+            setOrder([...order, newItem])    
+        }
+
     }
+
+    console.log(order)
 
     return {
         addItem
